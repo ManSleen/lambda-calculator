340480a480f0fc4b603d1e554c9c2982780e9bfb
Mike VanSleen <mike.vansleen@gmail.com>
2019-07-09 15:00:38 -0600

    Started styling components

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import NumberButton from "./NumberButton";
 
-//import any components needed
+import { numbers } from "../../../data";
 
-//Import your array data to from the provided data file
+import "../../../App.css";
 
 const Numbers = () => {
+  // const [theseNumbers, setTheseNumbers] = useState(numbers);
   // STEP 2 - add the imported data to state
   return (
-    <div>
-      {/* STEP 3 - Use .map() to iterate over your array data and return a button
-       component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+    <div className="numbers-container">
+      {numbers.map(number => (
+        <NumberButton number={number} />
+      ))}
     </div>
   );
 };
+
+export default Numbers;
--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -1,5 +1,10 @@
 import React from "react";
+import "../../App.css";
 
 const Display = () => {
-  return <div>{/* Display any props data here */}</div>;
+  return (
+    <div className="display-container">{/* Display any props data here */}</div>
+  );
 };
+
+export default Display;
